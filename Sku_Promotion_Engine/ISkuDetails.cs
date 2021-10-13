1ba4d2853a949bd6378d8e13f707f3327e12aba2
vishclue <vishclue@gmail.com>
2021-10-13 18:23:52 +0530

    Details: --introduced ISkuDetails indicating SkuToPrice details.

---
+++ Sku_Promotion_Engine/ISkuDetails.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sku_Promotion_Engine
+{
+    interface ISkuDetails
+    {
+        IDictionary<char, float> GetAllSkuPriceDetails();
+
+        void AddAdditionalSkus(char sku, float price);
+    }
+}
---
+++ Sku_Promotion_Engine/SkuDetails.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sku_Promotion_Engine
+{
+    class SkuDetails : ISkuDetails
+    {
+        private IDictionary<char, float> m_SkuToPriceDictionary;
+        public SkuDetails()
+        {
+            m_SkuToPriceDictionary = new Dictionary<char, float>();
+            m_SkuToPriceDictionary.Add('A',60);
+            m_SkuToPriceDictionary.Add('B', 50);
+            m_SkuToPriceDictionary.Add('C', 30);
+            m_SkuToPriceDictionary.Add('D', 20);
+
+        }
+        IDictionary<char, float> ISkuDetails.GetAllSkuPriceDetails()
+        {
+            return m_SkuToPriceDictionary;
+        }
+        
+        void ISkuDetails.AddAdditionalSkus(char sku, float price)
+        {
+            m_SkuToPriceDictionary.Add(sku, price);
+        }
+    }
+}
